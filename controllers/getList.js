5969bd59120d80ce36d0ced1aa35b2391b9c744b
tmd <123456>
2017-11-08 00:21:29 +0800

    操操操！ phatom不生效

--- controllers/getList.js
+++ controllers/getList.js
@@ -2,6 +2,8 @@ const getListModel = require('../models/getList');
 const charset = require ('superagent-charset');
 const superAgent = charset(require('superagent'));
 const cheerio =require('cheerio');
+const phantom = require('phantom');
+
 
 exports.getList = (req, res)=>{
 
@@ -37,32 +39,69 @@ exports.getList = (req, res)=>{
   list['是否支持双卡双待'] = '0';
   list['双卡制式'] = '';
   list['双卡SIM卡IMEI是否相同'] = '';
-
-
-
-  list['是否支持GPS'] = false;
-  list['是否支持NFC'] = false;
-
-
-
-
-  list['卡槽数量'] = 1;
-  list['通讯录是否支持名片式号码簿']=true;
-  list['通讯录是否支持名片式号码簿']=true;
-  list['是否支持蓝牙'] = false;
-  list['WIFI'] = false;
-  list['是否支持网络热点'] = false;
-  list['url'] = '';
-  list['价格'] = '';
-  list['子型号'] = '';
-  list['双卡制式'] = '';
-  list['操作系统版本'] = '';
-  list['CSFB']= '';
-  list['单卡双待'] = '';
-  list['上行载波聚合'] = '';
-  list['下行载波聚合']  = '';
-
-
+  list['是否支持OTA'] = '';
+  list['SIM卡电压'] = '';
+  list['微距镜头'] = '' ;
+  list['补光灯个数'] = '';
+  list['图像分辨率'] = '';
+  list['图像格式']  = '' ;
+  list['视频分辨率'] = '';
+  list['视频帧数(fps)'] = '';
+  list['软件版本(最新'] = '';
+  list['开发平台'] = '';
+  list['平台提供商'] = '';
+  list['平台版本'] = '';
+  list['数字基带'] = '';
+  list['物理层和协议栈软件'] = '';
+  list['模拟基带和射频'] = '';
+  list['手机存储空间大小'] = '';
+  list['用户可使用的最大内存空间'] = '';
+  list['存储卡类型'] = '';
+  list['存储卡最大容量'] = '';
+  list['通讯录是否支持名片式号码簿']= '是';
+  list['通讯录是否支持分组管理']= '是';
+  list['网络制式']= '';
+  list['制式等级'] ='';
+  list['USSD'] = '';
+  list['是否支持红外'] = '';
+  list['HDMI接口类型'] = '';
+  list['HDMI接口版本'] = '';
+  list['数据接口类型'] = '';
+  list['WAP版本']  = '';
+  list['是否支持彩信'] = '1';
+  list['是否支持Java'] = '';
+  list['支持的图像格式'] = '';
+  list['支持的声音格式'] = '';
+  list['是否有独立扬声器'] = '是';
+  list['是否支持mp3'] = '1';
+  list['播放器支持的音乐格式'] = '';
+  list['播放器支持的视频格式'] = '';
+  list['支持的邮件协议'] = '';
+  list['流媒体下载'] = '';
+  list['流媒体支持的声音格式'] = '';
+  list['流媒体支持的视频格式'] = '';
+  list['是否支持动态内存'] = '';
+  list['是否支持录音功能'] = '支持';
+  list['解锁方式'] = '';
+  list['关机闹铃'] = '支持';
+  list['重力感应'] = '';
+  list['距离感应器器'] = '支持';
+  list['电子罗盘'] = '支持';
+  list['FM收音机'] = '';
+  list['光线感应器'] = '';
+  list['长短信'] = '支持';
+  list['手机自带短信回执'] = '支持';
+  list['短信群发'] = '支持';
+  list['MM客户端'] = '0';
+  list['手机阅读'] = '0';
+  list['无线城市'] = '0';
+  list['G-plus游戏包'] = '0';
+  list['游戏'] = '支持';
+  list['是否支持显示附着状态'] = '';
+  list['是否支持显示激活状态'] = '';
+  list['是否支持显示数据传输'] = '';
+  list['author'] = 'spider';
+  
 
   superAgent.get('http://mobile.zol.com.cn/')
     .charset()
@@ -75,6 +114,24 @@ exports.getList = (req, res)=>{
             const $ = cheerio.load(success.text);
             let hrefParam =  $('#_j_tag_nav .nav__list li').eq(3).find('a').attr('href');
             let href = 'http://detail.zol.com.cn'+hrefParam;
+
+            // (async function(){
+            //   const instance = await phantom.create(['--ignore-ssl-errors=yes','--load-images=no','--disk-cache=yes']);
+            //   const page = await instance.createPage();
+            //   await page.on("onResourceRequested", function(requestData) {
+            //     console.info('Requesting', requestData.url)
+            //   });
+            //   const status = await page.open(href);
+            //   console.log(status);
+            //
+            //   const content = await page.property('content');
+            //   console.log(content);
+            //
+            //   await instance.exit();
+            //
+            // }());
+
+
             superAgent.get(href)
               .charset()
               .then((success)=>{
@@ -84,31 +141,40 @@ exports.getList = (req, res)=>{
                 list['品牌(英文)'] = $('#page-title').text().match(/(.+)\（/)[1].replace(new RegExp(list['厂商']),'');
                 list['型号'] = $('#page-title').text().match(/(.+)\（/)[1].replace(new RegExp(list['厂商']),'');
                 list['generalUrl'] = 'http://detail.zol.com.cn'+$('.breadcrumb a').eq(3).attr('href');
+
+
+
                 $('#newTb table li span').each((index, element)=>{
-                  if($(element).text() !== '') detail.push($(element).text())
+                  if($(element).text() !== '') detail.push($(element).text());
                 });
+
                 detail.forEach((item, index)=> {
+                  console.log(222222222);
                   switch(item){
                     case '支持频段':
-                      list['支持TD-LTE频段'] = detail[index + 1].match(/TD-LTE\s([^\\]+)\n/)[1];
+                      list['支持TD-LTE频段'] = detail[index + 1].includes('TD-LTE')? detail[index + 1].match(/TD-LTE\s([^\\]+)\n/)[1] :'';
+                      let i =0;
+                      list['网络制式'] = ['TD-SCDMA','GSM','CDMA','EDGE','WCDMA','CDMA2000','CDMA 1X',
+                        'CDMA EVDO'].reduce((pre, cur)=>{
+                          i++;
+                          if(detail[index + 1].includes(cur)){
+                            return pre.concat(i)
+                          }else{
+                            return pre
+                          }},[]).toString();
                       list['频段'] = detail[index + 1];
                       break ;
                     case '4G网络':
-                      list['网络制式'] =  detail[index + 1];
-                      list['是否支持FR'] = true;
-                      break ;
-                    case '3G网络':
-                      list['网络制式'] = list['网络制式'] ? list['网络制式'] + detail[index + 1] : detail[index + 1];
-                      list['终端支持能力'] = list['网络制式'] ? list['网络制式'] + detail[index + 1] : detail[index + 1];
+                      list['是否支持FR'] = '1';
                       break ;
                     case '导航':
-                      list['是否支持GPS'] = detail[index + 1].includes('GPS');
+                      list['是否支持GPS'] = detail[index + 1].includes('GPS') ? '1' : '0';
                       break;
                     case '连接与共享' :
-                      list['是否支持NFC'] = detail[index + 1].includes('NFC');
-                      //list['蓝牙协议集'] = detail[index + 1].match(/(蓝牙[^\，]+)/)[1];
-                      list['是否支持蓝牙'] = detail[index + 1].includes('蓝牙');
-                      //list['支持的蓝牙版本'] = detail[index + 1].match(/(蓝牙[^\，]+)/)[1];
+                      list['是否支持NFC'] = detail[index + 1].includes('NFC') ? '是' : '否';
+                      list['蓝牙协议集'] = '';
+                      list['是否支持蓝牙'] = detail[index + 1].includes('蓝牙')? '1' : '0';
+                      list['支持的蓝牙版本'] =detail[index + 1].includes('蓝牙')? detail[index + 1].match(/(蓝牙[^\，]+)/)[1] : '';
                       break;
                     case '手机尺寸':
                       list['机长(mm)'] = detail[index + 1].match(/(^[^x]+)/)[1];
@@ -119,10 +185,9 @@ exports.getList = (req, res)=>{
                       list['重量(g)'] = detail[index + 1].replace(/g/,'');
                       break;
                     case '造型设计':
-                      list['外观'] =  ['翻盖','直板','滑盖','侧滑盖','旋转屏','全键盘','全触屏'].findIndex(item=> item === detail[index + 1])+1    ;
+                      list['外观'] =  ['翻盖','直板','滑盖','侧滑盖','旋转屏','全键盘','全触屏'].findIndex(item=> item === detail[index + 1])+1+''   ;
                       break;
                     case '手机类型':
-
                       list['是否智能机'] = detail[index + 1].includes('智能') ? '1' : '0' ;
                       list['是否支持快速充电'] = detail[index + 1].includes('快充') ? '是' : '否';
                       break;
@@ -156,14 +221,14 @@ exports.getList = (req, res)=>{
                       list['主屏分辨率(纵)'] = detail[index + 1].match(/x([\d]+)/)[1];
                       break;
                     case '主屏材质':
-                      list['主屏材质'] = detail[index + 1];
-                      break;
-                    case '屏幕像素密度':
-                      list['屏幕像素密度'] = detail[index + 1];
+                      list['主屏材质'] = ['AMOLED','SLCD','TFT','ASV','IPS','其他','OGS'].findIndex(item=>detail[index + 1].includes(item))+1+'';
+
                       break;
                     case '机身接口':
                       list['充电器接口'] = detail[index + 1].replace(/^[^\，]+\，/, '');
                       list['耳机接口类型'] = detail[index + 1].includes('耳机')? detail[index + 1].match(/(^[^\，]+)\，/)[1] : undefined;
+                      list['USB接口版本'] = detail[index + 1].replace(/^[^\，]+\，/, '');
+
                       break;
                     case '电池容量':
                       list['电池容量'] = detail[index + 1];
@@ -183,46 +248,42 @@ exports.getList = (req, res)=>{
                       list['摄像头'] = detail[index + 1];
                       break;
                     case '后置摄像头':
-                      list['后置摄像头'] = detail[index + 1];
-                      break;
-                    case '前置摄像头':
-                      list['前置摄像头'] = detail[index + 1];
+                      list['摄像头像素(万像素)'] = detail[index + 1];
                       break;
                     case '闪光灯':
-                      list['闪光灯'] = detail[index + 1];
+                      list['闪光灯个数'] = detail[index + 1];
                       break;
                     case '拍照功能':
-                      list['拍照功能'] = detail[index + 1];
-                      break;
-                    case '视频拍摄':
-                      list['视频拍摄'] = detail[index + 1];
+                      list['照相功能'] = detail[index + 1];
                       break;
                     case '操作系统':
-                      list['操作系统'] = detail[index + 1];
+                      list['操作系统'] = ['ophone','windows mobile','windows ce','Android','symbian','linux','palm',
+                        'mkt','其他','iOS','Windows','blackberry','oms','meego'].findIndex(item=>detail[index + 1].includes(item))+1   ;
+                      list['操作系统版本'] = detail[index + 1];
                       break;
                     case 'RAM容量':
-                      list['RAM容量'] = detail[index + 1];
+                      list['RAM大小'] = detail[index + 1];
                       break;
                     case 'ROM容量':
-                      list['ROM容量'] = detail[index + 1];
+                      list['ROM大小'] = detail[index + 1];
                       break;
                     case '存储卡':
-                      list['存储卡'] = detail[index + 1];
+                      list['存储卡类型'] = '1';
                       break;
                     case '扩展容量':
-                      list['扩展容量'] = detail[index + 1];
+                      list['存储卡最大容量'] = detail[index + 1];
                       break;
                     case 'WLAN功能':
-                      list['WIFI'] = detail[index + 1].includes('WIFI');
+                      list['WIFI'] = detail[index + 1].includes('WIFI')?'1':'0';
                       list['WIFI支持协议'] = detail[index + 1];
-                      list['是否支持网络热点'] = detail[index + 1].includes('WIFI');
+                      list['是否支持网络热点'] = detail[index + 1].includes('WIFI')?'是':'否';
                       break;
                   }
                 });
-               // console.log(list);
-                getListModel.create(list ,(err)=>{
-                  if(err) console.log('从首页存入失败'+err);
-                });
+                //console.log(list);
+                // getListModel.create(list ,(err)=>{
+                //   if(err) console.log('从首页存入失败'+err);
+                // });
 
               });
 
@@ -241,7 +302,7 @@ exports.getPrice = (req, res) =>{
         const $ = cheerio.load(success.text);
         let price = $('#_j_local_price a.price').text();
         let fullName =  $('.product-model__name').text();
-        getListModel.update({'全称':fullName},{$set:{'价格':price}},(err,doc)=>{
+        getListModel.update({'全称':fullName},{$set:{'市场价格':price}},(err,doc)=>{
           console.log(doc);
         });
       })
