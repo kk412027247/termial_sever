840eb367e5b8efc1054f2a9aebbb0a2ac6dad27d
TMD <1@123.com>
2018-02-23 21:10:48 +0800

    修复下载无地址的bug

--- controllers/download.js
+++ controllers/download.js
@@ -3,10 +3,10 @@ const iconv = require('iconv-lite');
 const getListModel = require('../models/getList');
 const tacModel = require('../models/tac');
 
-//一次只能下载一个文件？？？
+
 exports.download = (req, res) =>{
   if(!req.query._id){
-    res.download('1');
+    res.download('./public/template/查无结果');
   }else{
     if((typeof req.query._id)==='string'){
       this.arr = [req.query._id]
@@ -37,23 +37,13 @@ exports.download = (req, res) =>{
           })
         }
       });
-      
-      // //node自带同步版本，虽然简答和直观,但会影响整个服务器的性能
-      // const buffer = fs.readFileSync(file1);
-      // //这里生成了GBK格式的文件给excel打开；
-      // fs.writeFileSync(file1,iconv.encode(buffer,'GB18030'));
-      // res.download(file1,()=>{
-      //   fs.unlinkSync(file1)
-      // });
     });
-
-    //console.log(`${file1}文件生成中`);
-  }
+    }
 };
 
 exports.downloadTac = (req, res) =>{
   if(!req.query._id){
-    res.download('1');
+    res.download('./public/template/查无结果');
   }else{
     if((typeof req.query._id)==='string'){
       this.arr = [req.query._id]
@@ -84,16 +74,7 @@ exports.downloadTac = (req, res) =>{
           })
         }
       });
-
-      // const buffer = fs.readFileSync(file2);
-      // //这里生成了GBK格式的文件给excel打开；
-      // fs.writeFileSync(file2,iconv.encode(buffer,'GB18030'));
-      //
-      // res.download(file2,()=>{
-      //   fs.unlinkSync(file2)
-      // });
     });
-    //console.log(`${file2}文件生成中`);
   }
 };
 
